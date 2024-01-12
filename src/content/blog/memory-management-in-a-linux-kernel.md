9f3ee27b38ed8d0c9af2b1b9168951124cae41b1
Anmol <ainsa2279@gmail.com>
2024-01-12 16:29:54 +0530

    Fix: Theme Colors

---
+++ src/content/blog/memory-management-in-a-linux-kernel.md
@@ -0,0 +1,9 @@
+---
+title: "Memory Management in a Linux Kernel"
+description: "---"
+keywords: "---"
+pubDate: "Jan 14 2024"
+isPublished: false
+heroImage: "/blog/must-know-linux-commands.webp"
+---
+
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,svg}',
-		'./public/**/*.svg',
-	],
- theme:{
-   extend:{ 
-    colors:{
-      primary:"#090B0C",
-      secondary:"#84BCEB",
-      accent:"#502CDD",
-      text:"#fff"
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,svg}",
+    "./public/**/*.svg",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#181825",
+        // primary:"#090B0C",
+        secondary: "#cba6f7",
+        // secondary: "#84BCEB",
+        accent: "#502CDD",
+        text: "#fff",
+      },
     },
-   },
- },  
-	plugins: [require('@tailwindcss/typography')],
-}
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
