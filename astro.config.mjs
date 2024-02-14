3f42dffeb620a95814c175d05720fb03def67071
Anmol <ainsa2279@gmail.com>
2024-02-14 18:25:04 +0530

    Add: PWA functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import AstroPWA from "@vite-pwa/astro";
 
 export default defineConfig({
   site: "https://anmol-fzr.web.app",
-  integrations: [sitemap(), tailwind(),  icon()],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    icon(),
+    AstroPWA(),
+  ],
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "viewport"
-  }
+    defaultStrategy: "viewport",
+  },
 });
