37fae160ae505d769dfca73166992660f1587fad
Anmol <ainsa2279@gmail.com>
2024-07-29 06:37:37 +0530

    Add Indra Kumar

--- src/config.ts
+++ src/config.ts
@@ -7,14 +7,14 @@ const EMAIL = data.email;
 const META = {
   TITLE: NAME + " | Expert Web Developer: Empowering Online Success",
   DESCRIPTION: `Transforming Ideas into Digital Realities: Experience personalized web development solutions tailored to your unique vision.
-Our professional web developer crafts captivating websites with a focus on user experience, responsiveness, and flawless functionality.
-Discover how we can bring your online presence to life. Contact us for a consultation today.`,
+Our professional web developer crafts captivating websites with a focus on user experience, responsiveness, and flawless functionality.`,
   KEYWORD: `javascript developer,web developer india, web developer means,google web developer,web developer gurgaon,web developer delhi,
   web developer punjab,web developer india,web developer banglore,web developer google, react developer, nextjs developer, tailwind css developer,
 web developer, frontend developer, software developer, UI/UX developer, responsive web design, web development services,web engineer,full stack 
 javascript developer,mern stack developer,certified javscript developer`,
   IMAGE: `/main.webp`,
-  GOOGLE_SITE_VERIFICATION: "HziuWubW8xm3rCiQYFmw68QweNGkemSE69e3QOXJEco"
+  // GOOGLE_SITE_VERIFICATION: "M7cRQ7-GP19WVgwgB811IV0CgWuSRW83k0L_gIcQy4I",
+  GOOGLE_SITE_VERIFICATION: "HziuWubW8xm3rCiQYFmw68QweNGkemSE69e3QOXJEco",
 } as const
 
 export { META, NAME, JOB_TITLE, EMAIL }
