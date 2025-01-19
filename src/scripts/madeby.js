8a09fb5e1bbe360025450b1a20269a6c18529fbb
Anmol <ainsa2279@gmail.com>
2025-01-20 02:03:13 +0530

    Add: Made By Script

---
+++ src/scripts/madeby.js
@@ -0,0 +1,38 @@
+class MadeBy extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const className = this.getAttribute("class") ?? ""
+    const linkClassName = this.getAttribute("link_class") ?? ""
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const wrapper = document.createElement("span");
+    wrapper.setAttribute("id", "wrapper")
+    wrapper.setAttribute("class", className)
+
+    const iconElem = document.createElement("span")
+    iconElem.setAttribute("class", "icon")
+    iconElem.textContent = "❤️ "
+
+    const info = document.createElement("p");
+
+    const linkElem = document.createElement("a")
+    linkElem.setAttribute("class", linkClassName)
+    linkElem.setAttribute("href", "https://withanmol.com")
+    linkElem.setAttribute("target", "_blank")
+    linkElem.textContent = "Anmol"
+
+    info.append("Made with ")
+    info.appendChild(iconElem)
+    info.append("by ")
+    info.appendChild(linkElem)
+
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(info);
+  }
+}
+
+customElements.define("made-by", MadeBy);
